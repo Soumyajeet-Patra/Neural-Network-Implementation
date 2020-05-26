af59f3d5d8a46ccafdf2c185b6874d5b79c3d865
Soumyajeet-Patra <soumyajeet.patra@gmail.com>
2020-05-26 14:46:13 -0700

    Update neuralnet.py

--- neuralnet.py
+++ neuralnet.py
@@ -1,12 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
-#
-# Copyright © 2016 erilyth <vishalvenkat71@gmail.com>
-#
-# Distributed under terms of the MIT license.
 
-"""
 
 """
 
